f653f25924501a00f304e4f60c32748d028ad551
Ilaiya <yoursilaiyar@gmail.com>
2026-06-05 00:27:02 +0530

    gradle kts

--- androidapp/settings.gradle.kts
+++ androidapp/settings.gradle.kts
@@ -1,2 +1,18 @@
-settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SMSSync"
 include(":app")
